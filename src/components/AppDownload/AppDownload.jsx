574f0adcc3e852268e0861dd412586c92ffe3461
banushasarma <sharmabanu03@gmail.com>
2024-08-09 22:03:19 +0530

    Responsive style media queries added.

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
 
 const AppDownload = () => {
-  return <div>AppDownload</div>;
+  return (
+    <div className="app-download" id="app-download ">
+      <p>
+        For Better Experience Download
+        <br />
+        Tomoto App
+        <div className="app-download-platforms">
+          <img src={assets.play_store} alt="" />
+          <img src={assets.app_store} alt="" />
+        </div>
+      </p>
+    </div>
+  );
 };
 
 export default AppDownload;
