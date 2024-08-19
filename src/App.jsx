7551c06a739772b1fce7c4255e2f5661f5d25a6f
banushasarma <sharmabanu03@gmail.com>
2024-08-19 12:04:18 +0530

    Order place completed.

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Verify from "./pages/Verify/Verify";
+import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
       {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
@@ -18,6 +21,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
       <Footer />
---
+++ src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MyOrders.css";
+
+const MyOrders = () => {
+  return <div>MyOrders</div>;
+};
+
+export default MyOrders;
